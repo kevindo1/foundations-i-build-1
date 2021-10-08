22350adaea1113b02ec67cb4396278e00fb92c95
Kevin Do <kevind0501@gmail.com>
2021-10-08 13:22:35 -0700

    added functioning random number generator

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import { random } from './random.js';
 
 // import functions and grab DOM elements
-const submitButton = document.getElementById('submit');
+const submitButton = document.getElementById('generate');
 const randomParagraph = document.getElementById('random-num');
+
 // initialize global state
 
 // set event listeners 
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
-console.log(random);
-submitButton = addEventListener('click', ()=> {
 
+submitButton.addEventListener('click', ()=> {
+    const result = random(0, 100);
+    randomParagraph.textContent = result;
 });
