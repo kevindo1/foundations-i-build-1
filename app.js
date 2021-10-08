5f844a0cdd7b0db7a86379c0b889b8448be72b16
Kevin Do <kevind0501@gmail.com>
2021-10-08 13:15:53 -0700

    added HTML

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// import functions and grab DOM elements
+import { random } from './random.js';
 
+// import functions and grab DOM elements
+const submitButton = document.getElementById('submit');
+const randomParagraph = document.getElementById('random-num');
 // initialize global state
 
 // set event listeners 
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
+console.log(random);
+submitButton = addEventListener('click', ()=> {
 
+});
